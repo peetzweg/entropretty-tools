35ee8028468dab80eef997de547cd3ae0a1c4a83
peetzweg/ <phil.czek@gmail.com>
2024-09-11 18:23:51 +0200

    reference local version, not published package

--- packages/entropretty-editor/src/worker.ts
+++ packages/entropretty-editor/src/worker.ts
@@ -1,5 +1,5 @@
 import * as Comlink from "comlink";
-import { createWorker } from "entropretty-editor";
+import { createWorker } from "@/lib/createWorker";
 
 Comlink.expose(
   createWorker(import.meta.glob(["./schemas/*.ts", "./schemas/*.js"]))
