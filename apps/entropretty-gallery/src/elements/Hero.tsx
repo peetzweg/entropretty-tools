91cc75b141755b989e2db23bb6ce93c7baa458f8
peetzweg/ <phil.czek@gmail.com>
2024-09-17 18:04:05 +0200

    updates hero text a bit

--- apps/entropretty-gallery/src/elements/Hero.tsx
+++ apps/entropretty-gallery/src/elements/Hero.tsx
@@ -21,12 +21,9 @@ export const Hero: React.FC = () => {
 
       <p>
         The new solution,{" "}
-        <b>
-          <i>'Proof-of-Ink'</i>
-        </b>{" "}
-        will enable users to prove their digital individuality in a
-        privacy-preserving manner through a unique tattoo serving as proof of
-        digital citizenship.
+        <span className="keyword-sans font-semibold">Proof of Ink</span>, will
+        enable users to prove their digital individuality in a
+        privacy-preserving manner through a unique tattoo serving as proof.
       </p>
 
       <div className="flex w-fit flex-row flex-wrap-reverse justify-start gap-x-4 gap-y-2">
