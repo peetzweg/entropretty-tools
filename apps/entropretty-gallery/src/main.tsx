bc41572edb3c1bf4f46ff09f01757cfc214ac9e6
pczek <839848+peetzweg@users.noreply.github.com>
2024-09-29 21:39:56 +0900

    add ScrollRestoration

--- apps/entropretty-gallery/src/main.tsx
+++ apps/entropretty-gallery/src/main.tsx
@@ -4,16 +4,37 @@ import { createRoot } from "react-dom/client"
 import Explore from "./routes/Explore.tsx"
 import LandingPage from "./routes/LandingPage.tsx"
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from "react-router-dom"
+
+function Root() {
+  return (
+    <>
+      <Outlet />
+
+      <ScrollRestoration />
+    </>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Explore />,
-  },
-  {
-    path: "/tools",
-    element: <LandingPage />,
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Explore />,
+      },
+      {
+        path: "/tools",
+        element: <LandingPage />,
+      },
+    ],
   },
 ])
 
