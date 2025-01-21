8330a3c3305e381480e489f5c680bb1bc4cf24eb
peetzweg/ <phil.czek@gmail.com>
2025-01-21 10:22:55 +0100

    use a proper seed in the editor

--- apps/entropretty-comp/src/features/create/CodeEditorPreview.tsx
+++ apps/entropretty-comp/src/features/create/CodeEditorPreview.tsx
@@ -1,11 +1,12 @@
 import { useAtom } from "jotai"
 import { useEffect, useMemo, useRef, useState } from "react"
-import { useWorker } from "../../contexts/worker-context"
-import { cn } from "../../lib/utils"
-import { editorCodeAtom, scriptErrorAtom } from "./atoms"
+import { useWorker } from "@/contexts/worker-context"
+import { cn } from "@/lib/utils"
+import { editorCodeAtom, editorSeedAtom, scriptErrorAtom } from "./atoms"
 
 const CodeEditorPreview = ({ size = 512, scale = 2 }) => {
   const [editorCode] = useAtom(editorCodeAtom)
+  const [seed] = useAtom(editorSeedAtom)
   const [, setScriptError] = useAtom(scriptErrorAtom)
 
   const [ready, setIsReady] = useState(false)
@@ -19,7 +20,7 @@ const CodeEditorPreview = ({ size = 512, scale = 2 }) => {
 
     artist.updateAlgorithm(0, editorCode)
     artist
-      .render(0, drawingSize, [1, 2, 3, 4])
+      .render(0, drawingSize, seed)
       .then((bitmap) => {
         const context = canvasRef.current!.getContext("2d")!
         context.clearRect(0, 0, drawingSize, drawingSize)
@@ -31,7 +32,7 @@ const CodeEditorPreview = ({ size = 512, scale = 2 }) => {
         setScriptError(e.message)
         console.error(e)
       })
-  }, [artist, drawingSize, editorCode, setScriptError])
+  }, [artist, drawingSize, editorCode, seed, setScriptError])
 
   return (
     <div className="flex h-full w-full items-center justify-center">
--- apps/entropretty-comp/src/features/create/atoms.ts
+++ apps/entropretty-comp/src/features/create/atoms.ts
@@ -1,6 +1,8 @@
 import { atom } from "jotai"
 import { AlgorithmView } from "@/lib/helper.types"
+import { getSeed } from "entropretty-utils"
 
 export const editorCodeAtom = atom<string>("")
+export const editorSeedAtom = atom<number[]>([...getSeed("Procedural")])
 export const scriptErrorAtom = atom<string | null>(null)
 export const remixAtom = atom<AlgorithmView | null>(null)
