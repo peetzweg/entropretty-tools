3a22d86f4a1e9bc210d29b6eceb326452f8cf4b7
pczek <839848+peetzweg@users.noreply.github.com>
2025-01-31 17:12:39 +0100

    remove unused schemas not sure who moved them here

--- apps/entropretty-comp/src/schemas/squares-mono.js
+++
@@ -1,9 +0,0 @@
-// Inspired by "Quadrate - Herbert Franke".
-
-import { doDraw } from "./squares-chroma";
-
-export const schema = {
-  draw: (c, s) => doDraw(c, s, true),
-  name: "Squares (Mono)",
-  artist: "ggwpez.gh",
-};
