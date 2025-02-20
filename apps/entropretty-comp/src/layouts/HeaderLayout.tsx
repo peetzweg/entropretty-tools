30f7980f8f2a12be4d1a3228a2a6c7e3a3dc215a
pczek <839848+peetzweg@users.noreply.github.com>
2025-02-20 23:12:58 +0200

    add X link

--- apps/entropretty-comp/src/layouts/HeaderLayout.tsx
+++ apps/entropretty-comp/src/layouts/HeaderLayout.tsx
@@ -58,6 +58,17 @@ export default function HeaderLayout() {
               <Link to="/mine">mine</Link>
             </Button>
           )}
+          <Button asChild variant={"link"}>
+            <Link to="https://x.com/entropretty" target="_blank">
+              <svg
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+                className="h-5 w-5 fill-current"
+              >
+                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+              </svg>
+            </Link>
+          </Button>
           <HelpMenu />
         </div>
 
