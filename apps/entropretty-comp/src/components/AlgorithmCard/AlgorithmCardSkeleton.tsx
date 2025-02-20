7d42992146fd3b4dc3573a7677f1d6083d47d09a
pczek <839848+peetzweg@users.noreply.github.com>
2025-02-20 23:34:43 +0200

    fix loading skeletons

--- apps/entropretty-comp/src/components/AlgorithmCard/AlgorithmCardSkeleton.tsx
+++ apps/entropretty-comp/src/components/AlgorithmCard/AlgorithmCardSkeleton.tsx
@@ -2,6 +2,6 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export function AlgorithmCardSkeleton() {
   return (
-    <Skeleton className="flex h-[438px] w-full max-w-[680px] rounded-none" />
+    <Skeleton className="flex h-[665px] w-screen rounded-none sm:h-[438px] sm:w-[680px]" />
   )
 }
